3dfa3eaf5d6422e106c60b527689c07558c92105
ahuiru <zhuangerlangyou@gmail.com>
2022-09-28 13:52:47 +0900

    Detailed Pokemon data for every single one

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 
 import { useEffect, useState } from 'react';
 import './App.css';
-import { getAllPokemon } from './utils/pokemon.js';
+import { getAllPokemon, getPokemon } from './utils/pokemon.js';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       //全てのポケモンデータの取得
       let res = await getAllPokemon(initialURL);
-      console.log(res);
+      //各ポケモンの詳細データを取得
+      loadPokemon(res.results)
+      // console.log(res.results)
       setLoading(false);
     };
     fetchPokemonData();
   }, [])
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        // console.log(pokemon)
+        let pokemonRecoad = getPokemon(pokemon.url)
+        return pokemonRecoad;
+      })
+    );
+    setPokemonData(_pokemonData)
+  };
+
+  console.log(pokemonData)
+
 
   return (
     <div className="App">
